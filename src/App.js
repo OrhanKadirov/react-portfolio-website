703ff19d77e1bf6a3b0b4373e8ab3d760200d163
root <root@DESKTOP-7KL1EG7>
2023-01-09 02:57:27 +0100

    responsive nav bar with hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar/navBar';
 
 function App() {
   return (
     <div className="App">
      <h1>React Portfolio App</h1>
+     <Navbar/>
     </div>
   );
 }
